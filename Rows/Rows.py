27bc5b4241fae3304c20ba960af319b663da332c
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-04-07 00:56:48 +0300

    ex_3 done

--- Rows/Rows.py
+++ Rows/Rows.py
@@ -1,1 +1,7 @@
-
+string = input('enter string:  ')
+src_symbol = input(' enter the symbol you are looking for:  ')
+count = 0
+for c in string:
+    if c == src_symbol:
+        count +=1
+print(count)

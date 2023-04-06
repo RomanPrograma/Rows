926c68e61681cd77a03d3d9f1ba988af97b2fbb0
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-04-07 00:24:46 +0300

    ex_2 done

--- Rows/Rows.py
+++ Rows/Rows.py
@@ -1,1 +1,13 @@
 
+string = input('enter str:  ')
+alpha_count=0
+number_count=0
+for i in string:
+    if i.isalpha():
+        alpha_count +=1
+    elif i.isdigit():
+        number_count +=1
+    else:
+        print('bl')
+print(alpha_count)
+print(number_count)

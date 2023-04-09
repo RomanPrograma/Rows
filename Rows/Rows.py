b67f6ce523830b029296204037c0f44077097318
Roman <roma2000kuba@gmail.com>
2023-04-09 16:28:43 +0300

    ex_5

--- Rows/Rows.py
+++ Rows/Rows.py
@@ -1,1 +1,7 @@
 
+
+
+string =input('enter string:  ')
+search_word =input('enter word what looking for:  ')
+word_for_change =input('enter replacement word:  ')
+print(string.replace(search_word,word_for_change))

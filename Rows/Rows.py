9ea246f88260a62adaa05545c82888a789110a9d
Roman <roma2000kuba@gmail.com>
2023-04-10 23:19:33 +0300

    ex_4 doneeeeee

--- Rows/Rows.py
+++ Rows/Rows.py
@@ -1,1 +1,7 @@
-
+string = input("Enter a string: ")
+word = input("Enter a word: ")
+count = 0
+for i in range(len(string)):
+    if string[i:i+len(word)] == word:
+        count += 1
+print("The word", word, "occurs", count, "times in the string", string)
